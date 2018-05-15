07c7461d7d5ad53dd6db876ce8713552de82f1c3
Cole Winstanley <crw2998@gmail.com>
2018-05-15 10:26:07 +0000

    full caching and multithreaded data load

--- data.py
+++ data.py
@@ -3,6 +3,8 @@ import numpy as np
 from scipy.misc import imread, imresize
 from tqdm import tqdm
 
+from multiprocessing.pool import ThreadPool
+
 import os
 import hashlib
 import _pickle as cPickle
@@ -17,17 +19,14 @@ TRAIN_DEV_TEST = (0.65, 0.20, 0.15)
 PATH = '/home/colewinstanley/231N-project'
 
 class Data(object):
-    def __init__(self, xpath=DEFAULT_X, yfile=DEFAULT_Y, cacheSmash=False):
+    def __init__(self, xpath=DEFAULT_X, yfile=DEFAULT_Y, cacheSmash=False, threads=8):      # 8 seems best on Google Cloud
         assert(sum(TRAIN_DEV_TEST) == 1.0)
-        self.X = None
-
-        initial_y = np.genfromtxt(yfile)
 
         h = hashlib.sha1(bytearray("".join(os.listdir(xpath)) + yfile + str(dims), 'utf-8')).hexdigest()
         p = os.path.join(PATH, "cache", h + ".pkl")
         useCache = os.access(p, os.R_OK) and not cacheSmash
         if useCache:
-            print('Attempting to load from cached pkl object:' + str(h))
+            print('Attempting to load from cached pkl object.')
             try:
                 with open(p, 'rb') as pkl:
                     self.X, self.y = cPickle.load(pkl)
@@ -36,27 +35,37 @@ class Data(object):
                 print ('Cache load failed.')
                 useCache = False
         if not useCache:
-            for image in tqdm(os.listdir(xpath)):
-                index = int(image[:6])
-                image_data = imread(os.path.join(xpath, image))
-                if len(image_data.shape) < 3: continue
-                image_data = imresize(image_data, dims)
-                if self.X is None:
-                    self.X = image_data[None]
-                    self.y = initial_y[index][None]
-                else:
-                    self.X = np.concatenate((self.X, image_data[None]))
-                    self.y = np.concatenate((self.y, initial_y[index][None]))
+            self.initial_y = np.genfromtxt(yfile)
+            files = [(int(image[:6]), os.path.join(xpath, image)) for image in os.listdir(xpath)]
+            pool = ThreadPool(threads)
+            self.loaded = 0
+            results = pool.map(self.get_image, files)
+            self.X = np.concatenate([d['X'] for d in results if d['X'].dtype != np.bool])       # using dtype as sentinel
+            self.y = np.concatenate([d['y'] for d in results if d['X'].dtype != np.bool])
+
             try:
                 with open(p, 'wb+') as pkl:
                     cPickle.dump((self.X, self.y), pkl)
             except:
                 print ('Cache dump failed.')
 
+            del self.initial_y
 
         self.num_examples = self.X.shape[0]
         np.random.seed(random_seed)
         self.indices = np.random.permutation(range(0, self.num_examples))
+
+    def get_image(self, index_file):
+        initial_y = self.initial_y
+        index, file = index_file
+        self.loaded += 1 # yeah yeah yeah concurrency issues
+        if self.loaded % 100 == 0: print('loaded ' + str(self.loaded))
+        image_data = imread(file)
+        if len(image_data.shape) < 3: return {'X': np.zeros(1, dtype=np.bool), 'y': np.zeros(1, dtype=np.bool)}
+        image_data = imresize(image_data, dims)
+        X = image_data[None]
+        y = initial_y[index][None]
+        return {'X': X, 'y': y}
 
     def get_train(self, split=TRAIN_DEV_TEST):
         return self.X[:int(self.num_examples*split[0])], self.y[:int(self.num_examples*split[0])]
